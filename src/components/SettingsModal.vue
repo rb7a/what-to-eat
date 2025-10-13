c0b0c4cb4a1c435cd25ee089d01ea518b4280804
liuziting <liuziting2014@163.com>
2025-10-13 13:09:39 +0800

    Update SettingsModal.vue

--- src/components/SettingsModal.vue
+++ src/components/SettingsModal.vue
@@ -140,7 +140,6 @@
                                             class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                                             placeholder="300000"
                                         />
-                                        <p class="text-xs text-gray-500 mt-1">API请求超时时间，建议300000毫秒(5分钟)</p>
                                     </div>
                                 </div>
                             </div>
